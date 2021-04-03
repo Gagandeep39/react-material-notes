282bf5f6dd8dd288d03d3491d370ec1e8bc576d1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-03 13:38:06 +0000

    Added a simple title

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,19 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 const Create = () => {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a new Note
+      </Typography>
+    </div>
+  );
 };
 
 export default Create;
