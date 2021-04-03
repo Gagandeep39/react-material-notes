2e554e0f45c3c64912f3490b22d05548d7d4ba19
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-03 21:09:09 +0000

    Added custom css

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,27 @@
-import { Button, Container, Typography } from '@material-ui/core';
+import { Button, Container, makeStyles, Typography } from '@material-ui/core';
 import { KeyboardArrowRight } from '@material-ui/icons';
 import React from 'react';
 
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 90,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue',
+    },
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  },
+});
+
 const Create = () => {
+  const classes = useStyles();
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -14,6 +30,7 @@ const Create = () => {
         Create a new Note
       </Typography>
       <Button
+        className={classes.btn}
         type="submit"
         variant="contained"
         color="secondary"
