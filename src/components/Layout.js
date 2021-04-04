c518a767ba8415874eb72ecbc5e4cd1b898544c3
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-04 09:38:10 +0000

    Created side navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,20 @@
-import { makeStyles } from '@material-ui/core';
+import { Drawer, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const drawerWidth = 240;
 const useStyles = makeStyles({
   page: {
     backgroundColor: '#f9f9f9',
     width: '100%',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: 'flex',
   },
 });
 
@@ -12,10 +22,20 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* TODO App Drawer */}
 
       {/* TODO Side drawer */}
+      <Drawer
+        variant="permanent"
+        className={classes.drawer}
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }} // Override drwer pper width(Width of drwer)
+      >
+        <div>
+          <Typography variant="h5">Todo App</Typography>
+        </div>
+      </Drawer>
 
       <div className={classes.page}>{children}</div>
     </div>
