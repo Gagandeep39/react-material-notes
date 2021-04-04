05c27d5d5d7d46516cea0d38f1dad3db953c2878
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-04 09:30:17 +0000

    Updated layout UI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,25 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles({
+  page: {
+    backgroundColor: '#f9f9f9',
+    width: '100%',
+  },
+});
+
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      {/* TODO App Drawer */}
+
+      {/* TODO Side drawer */}
+
+      <div className={classes.page}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
