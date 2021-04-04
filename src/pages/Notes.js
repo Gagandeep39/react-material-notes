d5beae42835593b365bbb88d57afd4d037c43c11
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-04 09:01:11 +0000

    Added notes inside cards

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,4 @@
-import { Grid, Paper } from '@material-ui/core';
+import { Container, Grid, Paper } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
 const Notes = () => {
@@ -10,28 +10,15 @@ const Notes = () => {
   }, []);
 
   return (
-    <div>
+    <Container>
       <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
+        {notes.map((note) => (
+          <Grid item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>{note.title} </Paper>
+          </Grid>
+        ))}
       </Grid>
-      {notes.map((note) => (
-        <p key={note.id}> {note.title} </p>
-      ))}
-    </div>
+    </Container>
   );
 };
 
